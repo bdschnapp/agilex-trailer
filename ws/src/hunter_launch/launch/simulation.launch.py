f67a849a14099dde53b36f9109343353c778830b
Ben <ben.schnapp@cplaneai.com>
2025-10-21 10:26:53 -0400

    Initial Commit

---
+++ ws/src/hunter_launch/launch/simulation.launch.py
@@ -0,0 +1,109 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler, SetEnvironmentVariable, IncludeLaunchDescription, ExecuteProcess
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import LaunchConfiguration, Command, FindExecutable, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.substitutions import FindPackageShare
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    use_sim_time = DeclareLaunchArgument('use_sim_time', default_value='true')
+    use_sim_time_cfg = LaunchConfiguration('use_sim_time')
+
+    # --- Gazebo ---
+    gazebo_params_file = os.path.join(get_package_share_directory("hunter_launch"), 'config', 'gazebo_params.yaml')
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+        launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+    )
+
+    # --- Robot description ---
+    robot_description_content = Command([
+        PathJoinSubstitution([FindExecutable(name="xacro")]), " ",
+        PathJoinSubstitution([FindPackageShare("hunter_description"), "description", "robot.urdf.xacro"]),
+    ])
+    robot_description = {"robot_description": ParameterValue(robot_description_content, value_type=None)}
+
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[robot_description, {'use_sim_time': use_sim_time_cfg}],
+    )
+
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic', 'robot_description', '-entity', 'hunter'],
+        output='screen',
+        parameters=[{'use_sim_time': use_sim_time_cfg}],
+    )
+
+    load_joint_state_broadcaster = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'joint_state_broadcaster'],
+        output='screen'
+    )
+    load_ackermann_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'ackermann_like_controller'],
+        output='screen'
+    )
+
+    hunter_description_path = os.path.join(get_package_share_directory('hunter_description'))
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', os.path.join(hunter_description_path, 'rviz/robot_view.rviz')],
+        output='screen',
+        parameters=[{'use_sim_time': use_sim_time_cfg}],
+        additional_env={"XDG_RUNTIME_DIR": "/tmp/xdg-runtime-rviz"},  # silence the runtime dir warning
+    )
+
+    # --- PCD Map Publisher (PointCloud2) ---
+    default_pcd_map_path = os.path.join(get_package_share_directory('hunter_launch'), 'maps', 'map.pcd')
+    pcd_to_cloud = Node(
+        package='pcl_ros',
+        executable='pcd_to_pointcloud',
+        name='pcd_to_pointcloud',
+        remappings=[('cloud_pcd', '/pcd_map')],  # output topic
+        parameters=[{'frame_id': 'world'}, {'use_sim_time': use_sim_time_cfg}, {'file_name': default_pcd_map_path}],
+        output='screen'
+    )
+
+    # --- Static TF: world -> map ---
+    static_world_to_map = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='static_world_to_map',
+        arguments=['0', '0', '0', '0', '0', '0', '1', 'world', 'map'],
+        output='screen'
+    )
+    # --- Static TF: map -> odom ---
+    static_map_to_odom = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='static_map_to_odom',
+        arguments=['0', '0', '0', '0', '0', '0', '1', 'map', 'odom'],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        use_sim_time,
+
+        # event chain for ros2_control controllers
+        RegisterEventHandler(OnProcessExit(target_action=spawn_entity, on_exit=[load_joint_state_broadcaster])),
+        RegisterEventHandler(OnProcessExit(target_action=load_joint_state_broadcaster, on_exit=[load_ackermann_controller])),
+
+        # Gazebo + robot
+        gazebo,
+        node_robot_state_publisher,
+        spawn_entity,
+        rviz,
+
+        # PCD map + TF
+        pcd_to_cloud,
+        static_world_to_map,
+        static_map_to_odom
+    ])
